6815b8bbd42b3ba703ce6076fde84b47f9bd06b1
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-12-25 22:35:48 -0500

    first commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+
+export class Server {
+
+    private app = express();
+
+    async start() {
+
+        //* Middlewares
+
+        //* Public Folder
+        this.app.use(express.static('./public'));
+
+        this.app.get('*', (req, res) => {
+            const indexPath = path.join(__dirname + '../../../public/index.html');
+            res.sendFile(indexPath);
+        });
+
+        this.app.listen(3000, () => {
+            console.log('Server running on', 3000);
+        });
+    };
+};
