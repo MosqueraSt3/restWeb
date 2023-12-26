335104c87e6a51939bedd3ccc9346aab635ccac3
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-12-26 11:55:38 -0500

    feat: update todo

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 
 const todos = [
-    { id: 1, text: 'Buy milk', createdAt: new Date() },
-    { id: 2, text: 'Buy bread', createdAt: new Date() },
-    { id: 3, text: 'Buy eggs', createdAt: new Date() },
+    { id: 1, text: 'Buy milk', completedAt: new Date() },
+    { id: 2, text: 'Buy bread', completedAt: null },
+    { id: 3, text: 'Buy eggs', completedAt: new Date() },
 ];
 
 export class TodoController {
@@ -23,9 +23,9 @@ export class TodoController {
      */
     public getTodoById = (req: Request, res: Response) => {
         const id = +req.params.id;
-        if (isNaN(id)) res.status(400).json({ error: `ID must be a number` });
+        if (isNaN(id)) return res.status(400).json({ error: `ID must be a number` });
         const todo = todos.find(todo => todo.id === id);
-        if (!todo) res.status(404).json({ error: `Todo with ${id} not found` });
+        if (!todo) return res.status(404).json({ error: `Todo with ${id} not found` });
         return res.json(todo);
     }
 
@@ -34,13 +34,33 @@ export class TodoController {
      */
     public createTodo = (req: Request, res: Response) => {
         const { text } = req.body;
-        if (!text) res.status(400).json({ error: 'Text is required' });
+        if (!text) return res.status(400).json({ error: 'Text is required' });
         const newTodo = {
             id: todos.length + 1,
             text,
-            createdAt: new Date(),
+            completedAt: null,
         };
         todos.push(newTodo);
         return res.json(newTodo);
+    }
+
+    /**
+     * updateTodo
+     */
+    public updateTodo = (req: Request, res: Response) => {
+        const id = +req.params.id;
+        if (isNaN(id)) return res.status(400).json({ error: `ID must be a number` });
+        
+        const todo = todos.find(todo => todo.id === id);
+        if (!todo) return res.status(404).json({ error: `Todo with ${id} not found` });
+
+        const { text, completedAt } = req.body;
+        
+        todo.text = text || todo.text;
+        (completedAt === 'null')
+            ? todo.completedAt = null
+            : todo.completedAt = new Date(completedAt || todo.completedAt)
+
+        return res.json(todo);
     }
 }
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -9,7 +9,9 @@ export class TodoRoutes {
         const todoController = new TodoController();
 
         router.get('/', todoController.getTodos);
-        router.get('/:id', todoController.getTodos);
+        router.get('/:id', todoController.getTodoById);
+        router.post('/', todoController.createTodo);
+        router.put('/:id', todoController.updateTodo);
 
         return router;
     }
