22613209b9f7d3e4b78cdfd5fa2341160ddd4efe
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-12-26 11:35:41 -0500

    feat: create todo

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -18,14 +18,29 @@ export class TodoController {
         return res.json(todos);
     }   
 
-     /**
+    /**
      * getTodoById
      */
-     public getTodoById = (req: Request, res: Response) => {
+    public getTodoById = (req: Request, res: Response) => {
         const id = +req.params.id;
         if (isNaN(id)) res.status(400).json({ error: `ID must be a number` });
         const todo = todos.find(todo => todo.id === id);
         if (!todo) res.status(404).json({ error: `Todo with ${id} not found` });
         return res.json(todo);
+    }
+
+    /**
+     * createTodo
+     */
+    public createTodo = (req: Request, res: Response) => {
+        const { text } = req.body;
+        if (!text) res.status(400).json({ error: 'Text is required' });
+        const newTodo = {
+            id: todos.length + 1,
+            text,
+            createdAt: new Date(),
+        };
+        todos.push(newTodo);
+        return res.json(newTodo);
     }
 }
